23c4b0402a79f446543b7be1df305f53d1ca4937
rg <rgenius@yandex.ru>
2024-01-27 12:54:47 +0300

    Add lesson9

---
+++ flask_lessons/lesson9.1.py
@@ -0,0 +1,29 @@
+# Logging 
+    # Роли
+    # Сессии
+    # Куки
+
+# - JSON
+
+# SQLAlchemy 
+    # методы обработки данных в классе User
+
+# Deployment
+    # Выбор хостинга
+        # Heroku
+        # Pythonanywhere
+        # Amazon AWS
+    # Поддомены subdomen.site.ru
+    # HTTP Error code 404, 403, 500 ...
+
+
+# План:
+    # ревью SQLAlchemy 
+    # функционала парсинга
+
+
+# ДЗ
+    # Flask-WTF
+    # Logging
+    # Flask-Mail
+    # Extensions
